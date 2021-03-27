47eb76970e73fc24d4a5d29d86d788f220598b3d
G Bhanuteja <45899803+gbhanuteja23@users.noreply.github.com>
2021-03-28 02:04:48 +0530

    UI Controller Script

---
+++ Open-Source Learning Assistant AR/Assets/Scripts/UIController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Script for controlling the UI
+
+public class UIController : MonoBehaviour
+{
+    public GameObject menuUI;
+
+    public GameObject credits;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //Function for opening Credits Panel
+    public void OpenCredits()
+    {
+        menuUI.SetActive(false);
+
+        credits.SetActive(true);
+
+    }
+
+    //Function for going back to Main Menu
+    public void GoBack()
+    {
+        credits.SetActive(false);
+
+        menuUI.SetActive(true);
+
+    }
+
+    //Function for quitting the APP
+    public void ExitAPP()
+    {
+        Application.Quit();
+
+    }
+
+}
