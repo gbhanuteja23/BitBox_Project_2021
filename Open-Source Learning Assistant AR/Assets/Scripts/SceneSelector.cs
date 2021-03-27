6b1b8e4b1c11ed41d157e349074dd8e479448bdc
G Bhanuteja <45899803+gbhanuteja23@users.noreply.github.com>
2021-03-28 02:04:37 +0530

    Scene Selector Script

---
+++ Open-Source Learning Assistant AR/Assets/Scripts/SceneSelector.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+//Script for Selecting various Scenes in APP 
+
+public class SceneSelector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Function for starting the APP
+    public void StartAPP()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+
+    //Function for going back to Main Menu Scene 
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene(0); 
+
+    }
+
+}
